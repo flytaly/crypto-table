feb091728a6a9453ae59e3530678598395a4598a
Vitaly Yerofeyevsky <flytaly@gmail.com>
2018-09-07 12:06:31 +0300

    Replace Multigrid component with Grid and ScrollSync

--- client/src/components/currency-table/currency-table.js
+++ client/src/components/currency-table/currency-table.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { MultiGrid, AutoSizer } from 'react-virtualized';
-import { connect } from 'react-redux';
-import get from 'lodash.get';
+import * as React from 'react';
 import cn from 'classnames';
+import PropTypes from 'prop-types';
 import scrollbarSize from 'dom-helpers/util/scrollbarSize';
-
-import { entitiesSelector as tickerSelector } from '../../ducks/tickers';
-import { rowsSelector, columnsSelector, deleteColumn, deleteRow } from '../../ducks/selected';
+import { AutoSizer, Grid, ScrollSync } from 'react-virtualized';
 import './currency-table.less';
-import AddTableField from './add-table-field';
-import DeleteIcon from './delete-icon';
 
+export default class CurrencyTable extends React.PureComponent {
+    constructor(props, context) {
+        super(props, context);
 
-class CurrencyTable extends Component {
-    constructor(props) {
-        super(props);
-        this.myRef = React.createRef();
+        this.leftGrid = React.createRef();
+        this.rightGrid = React.createRef();
+        this.rightTopGrid = React.createRef();
+
         this.state = {
             hoveredRowIndex: null,
             hoveredColumnIndex: null,
-            clickedCol: null,
-            clickedRow: null,
 
             // Sizes
             columnWidth: 130,
@@ -31,62 +25,37 @@ class CurrencyTable extends Component {
         };
     }
 
+
     componentDidUpdate() {
-        this.myRef.current.forceUpdateGrids();
+        this.leftGrid.current.forceUpdate();
+        this.rightTopGrid.current.forceUpdate();
+        this.rightGrid.current.forceUpdate();
     }
+
 
     mouseLeaveHandler = () => this.setState({
         hoveredRowIndex: null,
         hoveredColumnIndex: null,
     });
 
-    cellRenderer = ({ columnIndex, key, rowIndex, style }) => {
-        const { rows, columns, tickers } = this.props;
+    mouseOverHandler = (rowIdx, colIdx) => this.setState({
+        hoveredRowIndex: rowIdx,
+        hoveredColumnIndex: colIdx,
+    });
 
-        const onMouseOver = () => this.setState({
-            hoveredRowIndex: rowIndex,
-            hoveredColumnIndex: columnIndex,
-        });
+    renderLeftHeaderCell = ({ key, style }) => (
+        <div // eslint-disable-line jsx-a11y/mouse-events-have-key-events
+            style={style}
+            key={key}
+            onMouseOver={() => this.mouseOverHandler(null, null)}
+        />);
 
-        if (columnIndex === 0) {
-            return this.renderLeftCell({
-                key,
-                rowIndex,
-                style,
-                onMouseOver,
-            });
-        }
-        if (rowIndex === 0) {
-            return this.renderHeaderCell({
-                key,
-                columnIndex,
-                style,
-                onMouseOver,
-            });
-        }
-
-        const baseAsset = rows[rowIndex - 1];
-        const { exchange, quoteAsset } = columns[columnIndex - 1];
-
-        const className = cn({
-            'grid-cell': true,
-            'hovered-row': rowIndex === this.state.hoveredRowIndex,
-        });
-
-        return (
-            <div className={className} key={key} style={style} onMouseOver={onMouseOver}>
-                {get(tickers, [exchange, baseAsset, quoteAsset, 'last_price'])}
-            </div>
-        );
-    };
-
-
-    renderLeftCell = ({ key, rowIndex, style, onMouseOver }) => {
+    renderLeftCell = ({ key, rowIndex, style }) => {
         const { rows } = this.props;
 
         const className = cn({
             'grid-left-cell': true,
-            'hovered-row': rowIndex && rowIndex === this.state.hoveredRowIndex,
+            'hovered-row': rowIndex === this.state.hoveredRowIndex,
         });
 
         return (
@@ -96,23 +65,19 @@ class CurrencyTable extends Component {
                 style={style}
                 tabIndex={0}
                 role="rowheader"
-                onMouseOver={onMouseOver}
+                onMouseOver={() => this.mouseOverHandler(rowIndex, null)}
                 onFocus={() => this.setState({ hoveredRowIndex: rowIndex })}
                 onBlur={() => this.setState({ hoveredRowIndex: null })}
-                onClick={() => this.setState({ clickedRow: this.state.clickedRow ? null : rowIndex })}
-                onMouseLeave={() => this.setState({ clickedRow: null })}
             >
-                {this.state.clickedRow === rowIndex && rowIndex
-                    ? <DeleteIcon onClick={() => this.props.deleteRow(rowIndex - 1)} />
-                    : null}
-                {rowIndex ? rows[rowIndex - 1] : ''}
+                {rows[rowIndex]}
             </div>
         );
     };
 
-    renderHeaderCell = ({ key, columnIndex, style, onMouseOver }) => {
+    renderHeaderCell = ({ key, columnIndex, style }) => {
         const { columns } = this.props;
-        const { exchange, quoteAsset } = columns[columnIndex - 1];
+        const { exchange, quoteAsset } = columns[columnIndex];
+
         const className = cn({
             'grid-header-cell': true,
             'hovered-column': columnIndex === this.state.hoveredColumnIndex,
@@ -125,17 +90,31 @@ class CurrencyTable extends Component {
                 style={style}
                 tabIndex={0}
                 role="columnheader"
-                onMouseOver={onMouseOver}
+                onMouseOver={() => this.mouseOverHandler(null, columnIndex)}
                 onFocus={() => this.setState({ hoveredColumnIndex: columnIndex })}
                 onBlur={() => this.setState({ hoveredColumnIndex: null })}
-                onClick={() => this.setState({ clickedCol: this.state.clickedCol ? null : columnIndex })}
-                onMouseLeave={() => this.setState({ clickedCol: null })}
             >
-                {this.state.clickedCol === columnIndex
-                    ? <DeleteIcon onClick={() => this.props.deleteColumn(columnIndex - 1)} />
-                    : null}
                 <div className="quoteAsset">{quoteAsset}</div>
                 <div className="exchange">{exchange}</div>
+            </div>
+        );
+    };
+
+    renderBodyCell = ({ columnIndex, key, rowIndex, style }) => {
+        const className = cn({
+            'grid-cell': true,
+            'hovered-row': rowIndex === this.state.hoveredRowIndex,
+        });
+
+        return (
+
+            <div // eslint-disable-line jsx-a11y/mouse-events-have-key-events
+                className={className}
+                key={key}
+                style={style}
+                onMouseOver={() => this.mouseOverHandler(rowIndex, columnIndex)}
+            >
+                {'0000.00000000'}
             </div>
         );
     };
@@ -144,71 +123,152 @@ class CurrencyTable extends Component {
         const { rows, columns } = this.props;
         const {
             columnWidth,
-            leftColumnWidth,
             rowHeight,
             headerRowHeight,
+            leftColumnWidth,
         } = this.state;
 
-        const totalWidth = (columns.length * columnWidth) + leftColumnWidth;
-        const totalHeight = (rows.length * rowHeight) + headerRowHeight + scrollbarSize();
+        const columnCount = columns.length;
+        const rowCount = rows.length;
 
-        const getColWidth = ({ index }) => index ? columnWidth : leftColumnWidth;
-        const getRowHeight = ({ index }) => index ? rowHeight : headerRowHeight;
+        const totalWidth = (columnCount * columnWidth) + leftColumnWidth;
+        const totalHeight = (rowCount * rowHeight) + headerRowHeight + scrollbarSize();
+        const bodyHeight = totalHeight - headerRowHeight;
+
 
         return (
-            <AutoSizer disableHeight>
-                {({ width: maxWidth }) => {
-                    const width = maxWidth < totalWidth ? maxWidth : totalWidth;
+            <ScrollSync>
+                {({ onScroll, scrollLeft, scrollTop }) => (
+                    <div
+                        className="grid-container"
+                        style={{ height: totalHeight }}
+                        onMouseLeave={this.mouseLeaveHandler}
+                    >
+                        <AutoSizer disableHeight>
+                            {({ width }) => {
+                                const bodyWidth = width < totalWidth
+                                    ? width - leftColumnWidth
+                                    : totalWidth - leftColumnWidth;
 
-                    return (
-                        <div style={{ width }}>
-                            <div onMouseLeave={this.mouseLeaveHandler}>
-                                <MultiGrid
-                                    ref={this.myRef}
-                                    fixedColumnCount={1}
-                                    fixedRowCount={1}
-                                    cellRenderer={this.cellRenderer}
-                                    columnWidth={getColWidth}
-                                    columnCount={columns.length + 1}
-                                    height={totalHeight}
-                                    rowHeight={getRowHeight}
-                                    rowCount={rows.length + 1}
-                                    width={width}
+                                return (
+                                    <React.Fragment>
+                                        {/* LEFT CORNER HEADER */}
+                                        <div
+                                            className="grid-container-left"
+                                            style={{
+                                                height: headerRowHeight,
+                                                width: leftColumnWidth,
+                                            }}
+                                        >
+                                            <Grid
+                                                cellRenderer={this.renderLeftHeaderCell}
+                                                className="grid-header-corner"
+                                                width={leftColumnWidth}
+                                                height={headerRowHeight}
+                                                rowHeight={headerRowHeight}
+                                                columnWidth={leftColumnWidth}
+                                                rowCount={1}
+                                                columnCount={1}
+                                            />
+                                        </div>
 
-                                    classNameTopRightGrid="grid-header"
-                                    classNameTopLeftGrid="grid-header-corner"
-                                    classNameBottomLeftGrid="grid-left"
-                                    classNameBottomRightGrid="grid-body"
-                                    styleBottomRightGrid={{ outline: 'none' }}
+                                        {/* LEFT COLUMN */}
+                                        <div
+                                            className="grid-container-left"
+                                            style={{
+                                                top: headerRowHeight,
+                                                width: leftColumnWidth,
+                                            }}
+                                        >
+                                            <Grid
+                                                className="grid-left-row"
+                                                cellRenderer={this.renderLeftCell}
+                                                columnWidth={leftColumnWidth}
+                                                columnCount={1}
+                                                height={bodyHeight}
+                                                rowHeight={rowHeight}
+                                                rowCount={rowCount}
+                                                scrollTop={scrollTop}
+                                                width={leftColumnWidth}
+                                                ref={this.leftGrid}
+                                            />
+                                        </div>
 
-                                    enableFixedColumnScroll
-                                    enableFixedRowScroll
+                                        {/* HEADER RIGHT */}
+                                        <div
+                                            className="grid-container-right"
+                                            style={{
+                                                left: leftColumnWidth,
+                                                height: headerRowHeight,
+                                                width: bodyWidth,
+                                            }}
+                                        >
+                                            <Grid
+                                                className="grid-header"
+                                                columnWidth={columnWidth}
+                                                columnCount={columnCount}
+                                                height={headerRowHeight}
+                                                cellRenderer={this.renderHeaderCell}
+                                                rowHeight={headerRowHeight}
+                                                rowCount={1}
+                                                scrollLeft={scrollLeft}
+                                                width={bodyWidth}
+                                                ref={this.rightTopGrid}
+                                            />
+                                        </div>
 
-                                    hideTopRightGridScrollbar
-                                />
-                            </div>
-                            <AddTableField />
-                        </div>
-                    );
-                }}
-            </AutoSizer>
+                                        {/* TABLE BODY */}
+                                        <div
+                                            className="grid-container-right"
+                                            style={{
+                                                left: leftColumnWidth,
+                                                top: headerRowHeight,
+                                                height: bodyHeight,
+                                                width: bodyWidth,
+                                            }}
+                                        >
+                                            <Grid
+                                                className="grid-body"
+                                                columnWidth={columnWidth}
+                                                columnCount={columnCount}
+                                                height={bodyHeight}
+                                                onScroll={onScroll}
+                                                cellRenderer={this.renderBodyCell}
+                                                rowHeight={rowHeight}
+                                                rowCount={rowCount}
+                                                width={bodyWidth}
+                                                ref={this.rightGrid}
+                                            />
+                                        </div>
+                                    </React.Fragment>
+                                );
+                            }
+                            }
+                        </AutoSizer>
+                    </div>
+                )}
+            </ScrollSync>
         );
     }
 }
 
+
 CurrencyTable.propTypes = {
-    rows: PropTypes.arrayOf(PropTypes.string).isRequired,
-    columns: PropTypes.arrayOf(PropTypes.object).isRequired,
-    tickers: PropTypes.shape({}).isRequired,
-    deleteColumn: PropTypes.func.isRequired,
-    deleteRow: PropTypes.func.isRequired,
+    rows: PropTypes.arrayOf(PropTypes.string),
+    columns: PropTypes.arrayOf(PropTypes.object),
 };
 
-export default connect(state => ({
-    tickers: tickerSelector(state),
-    rows: rowsSelector(state),
-    columns: columnsSelector(state),
-}), {
-    deleteColumn,
-    deleteRow,
-})(CurrencyTable);
+CurrencyTable.defaultProps = {
+    rows: ['---', '---', '---', '---', '---', '---', '---', '---', '---', '---', '---'],
+    columns: [{
+        quoteAsset: '---',
+        exchange: '-------',
+    }, {
+        quoteAsset: '---',
+        exchange: '-------',
+    }, {
+        quoteAsset: '----',
+        exchange: '-------',
+    }, {}, {}, {}],
+};
+

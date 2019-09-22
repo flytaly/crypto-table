2ab0fdc9ddbdca50e747f14f7da00c057cf3d207
Vitaly Yerofeyevsky <flytaly@gmail.com>
2019-09-22 21:48:02 +0300

    Compose enhancer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,15 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import reducer from './reducer';
 import rootSaga from './saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(sagaMiddleware),
+    composeEnhancer(applyMiddleware(sagaMiddleware)),
 );
 
 sagaMiddleware.run(rootSaga);

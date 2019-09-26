5d9bda1458bf2e007b081d7948ace1fb0c29dd50
Vitaly Yerofeyevsky <flytaly@gmail.com>
2019-09-26 21:47:53 +0300

    Update historical route

--- server/routes/historical.js
+++ server/routes/historical.js
@@ -1,9 +1,24 @@
 const cc = require('cryptocompare');
+const moment = require('moment');
 
-// Map params request to cryptocompare API
+const stepsDefault = 10;
+const timeUnits = {
+    months: 'months',
+};
+
 module.exports = async (req, res) => {
-    const { fsym, tsym } = req.query;
-    const date = new Date(Number(req.query.date));
-    const historicalData = await cc.priceHistorical(fsym, tsym, date);
-    res.send(historicalData);
+    const { fsym, tsym, steps = stepsDefault, timeUnit = timeUnits.months } = req.query;
+
+    const pricePromises = Array.from({ length: steps },
+        async (_, i) => {
+            const date = moment()
+                .subtract(i, timeUnit)
+                .toDate();
+            const result = await cc.priceHistorical(fsym, tsym, date);
+            return { ...result, time: date };
+        });
+
+    const results = await Promise.all(pricePromises);
+
+    res.send(results);
 };

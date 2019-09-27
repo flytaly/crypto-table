f2d888579644dc47480023595d84acf8a10c635c
Vitaly Yerofeyevsky <flytaly@gmail.com>
2019-09-27 23:33:45 +0300

    Fetch data in range

--- server/routes/historical.js
+++ server/routes/historical.js
@@ -2,18 +2,23 @@ const cc = require('cryptocompare');
 const moment = require('moment');
 
 const stepsDefault = 10;
-const timeUnits = {
-    months: 'months',
-};
 
 module.exports = async (req, res) => {
-    const { fsym, tsym, steps = stepsDefault, timeUnit = timeUnits.months } = req.query;
+    const { fsym, tsym, steps = stepsDefault } = req.query;
+
+    const startDate = req.query.startDate
+        ? new Date(Number(req.query.startDate))
+        : moment().subtract(1, 'years').toDate();
+
+    const endDate = req.query.endDate
+        ? new Date(Number(req.query.endDate))
+        : new Date();
+
+    const stepRange = (endDate - startDate) / steps;
 
     const pricePromises = Array.from({ length: steps },
         async (_, i) => {
-            const date = moment()
-                .subtract(i, timeUnit)
-                .toDate();
+            const date = new Date(endDate.getTime() - i * stepRange);
             const result = await cc.priceHistorical(fsym, tsym, date);
             return { ...result, time: date.getTime() };
         });
